61ee43bb101211383c781f3561e67d62866e9360
MrPipsterr <farrelpalinggi@gmail.com>
2023-06-20 23:13:22 +0800

    Project-2

--- src/App.java
+++
@@ -1,8 +0,0 @@
-import com.layouts.Menu;
-
-public class App {
-
-    public static void main(String[] args) {
-        Menu.showMenu();
-    }
-}
--- src/com/layouts/ReadData.java
+++
@@ -1,9 +0,0 @@
-package com.layouts;
-
-import com.controllers.DbController;
-
-public class ReadData {
-    public static void readData() {
-        DbController.getDatabase();
-    }  
-}
